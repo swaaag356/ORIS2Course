0161157b6512d2931265a9f3073ef352304f365a
Ilnaz Galiev <qazxswedc20063006@gmail.com>
2025-09-20 14:11:14 +0300

    lab01:second_commit

--- lab01/src/main/java/httpserver/parsing/Request.java
+++ lab01/src/main/java/httpserver/parsing/Request.java
@@ -6,17 +6,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Request {
-    private String method;            // GET, POST, DELETE...
-    private String resource;               // /home или /resource/part
-    private Map<String, String> params = new HashMap<>(); // query параметры
-    private String protocol;          // HTTP/1.1
+    private String method;
+    private String resource;
+    private Map<String, String> params = new HashMap<>();
+    private String protocol;
 
     public Request (BufferedReader reader) throws IOException {
         parse(reader);
     }
 
     private void parse(BufferedReader reader) throws IOException {
-        // Читаем первую строку: "GET /home?name=tat HTTP/1.1"
         String requestLine = reader.readLine();
         if (requestLine == null || requestLine.isEmpty()) {
             throw new IOException("Empty request");
@@ -27,7 +26,6 @@ public class Request {
         String resourceWithParams = parts[1];
         this.protocol = parts[2];
 
-        // Делим на RESOURCES и query string
         String[] resourceParts = resourceWithParams.split("\\?", 2);
         this.resource = resourceParts[0];
 
@@ -42,7 +40,6 @@ public class Request {
         }
     }
 
-    // --- Getters ---
     public String getMethod() { return method; }
     public String getResource() { return resource; }
     public String getProtocol() { return protocol; }
